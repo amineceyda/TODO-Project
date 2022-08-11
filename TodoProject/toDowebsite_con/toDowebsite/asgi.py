992c86c97b9555244408fbe0d998479d74c3ef06
amineceyda <amineceyda33@gmail.com>
2022-08-11 18:10:35 +0300

    template oluşturuldu

--- TodoProject/toDowebsite_con/toDowebsite/asgi.py
+++ TodoProject/toDowebsite_con/toDowebsite/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for toDowebsite project.
+ASGI config for todowebsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toDowebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todowebsite.settings')
 
 application = get_asgi_application()
--- TodoProject/toDowebsite_con/toDowebsite/urls.py
+++ TodoProject/toDowebsite_con/toDowebsite/urls.py
@@ -1,7 +1,7 @@
-"""toDowebsite URL Configuration
+"""todowebsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include  # add this
 
 urlpatterns = [
-    path('', include('toDoapp.urls')),
-    path('admin/', admin.site.urls),
-    
+    path('admin/', admin.site.urls),          # Django admin route
+    path("", include("apps.authentication.urls")), # Auth routes - login / register
+    path("", include("apps.home.urls"))             # UI Kits Html files
 ]
